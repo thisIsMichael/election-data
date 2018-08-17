c855e952819fd2fa4ece10222a0d51f4cb2cb4c8
thisIsMichael <m1chael0@hotmail.co.uk>
2018-08-17 16:16:44 +0100

    trying to get uk map to work

--- js/graph.js
+++ js/graph.js
@@ -1,9 +1,44 @@
 var w = 300;
 var h = 800;
 
-d3.select("body")
-    .append("svg")
-    .attr({
+var svg = d3.select("body")
+    .append("svg");
+
+svg.attr({
         width: w,
         height: h
-    })
+    });
+
+
+var projection = d3.geo.albers();
+/*.translate([w/2, h/2])
+.scale([500]);*/
+
+var path = d3.geo.path(projection);
+/*
+d3.json("geojson/uk-constituencies.json").then(function(json){
+    console.log("data loaded");
+
+    svg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr({
+            d: path,
+            "fill": "gray"
+        })
+});*/
+
+
+d3.json("geojson/uk-constituencies.json", function(data){
+    var x = true;
+
+    svg.selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+        .attr({
+            d: path,
+            "fill": "gray"
+        })
+});
